4e830cc73a3a17a35aa8f388db0e8473d355430f
Lelya2210 <117176572+Lelya2210@users.noreply.github.com>
2022-12-16 00:05:35 +0300

    Update test_pet_friends.py

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,5 +1,5 @@
 from api import PetFriends
-from settings import valid_email, valid_password
+from settings import valid_email, valid_password, incorrect_key
 import os
 
 pf = PetFriends()
@@ -8,8 +8,6 @@ def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
     status, result = pf.get_api_key(email, password)
     assert status == 200
     assert 'key' in result
-
-
 
 def test_get_list_of_pets(filter=''):
     _, auth_key = pf.get_api_key(valid_email, valid_password)
@@ -34,9 +32,6 @@ def test_post_add_photo_to_pet(pet_photo='images/rysch_kot.jpg'):
 
     # Берём id первого питомца из списка и отправляем запрос на добавление фото
     pet_id = my_pets['pets'][1]['id']
-    #status, result = pf.post_add_photo_to_pet(auth_key, pet_photo, pet_id)
-    #assert status == 200
-    #assert result['id'] == pet_id
 
     if len(my_pets['pets']) > 0:
         status, result = pf.post_add_photo_to_pet(auth_key, pet_photo, pet_id)
@@ -45,5 +40,30 @@ def test_post_add_photo_to_pet(pet_photo='images/rysch_kot.jpg'):
         assert status == 200
         assert result['id'] == pet_id
     else:
-        # если спиок питомцев пустой, то выкидываем исключение с текстом об отсутствии своих питомцев
+        # если спиcок питомцев пустой, то выкидываем исключение с текстом об отсутствии своих питомцев
         raise Exception("There is no my pets")
+
+# Негативный тест (неверный api-ключ)
+def test_FAILD_get_list_of_pets_with_incorrect_key(filter=''):
+        _, auth_key = pf.get_api_key(valid_email, valid_password)
+        assert auth_key == incorrect_key
+
+# Негативный тест (пустые поля логин и пароль):
+def test_FAILD_get_api_key_for_empty_user_data(email='', password=''):
+        status, result = pf.get_api_key(email, password)
+
+        if len(email or password) == 0:
+            assert status == 200
+        else:
+            raise Exception('Data is invalid. Please use correct data for new user.')
+
+#Проверим, что все обязательные поля для обновления информации заполнены корректно.
+def test_update_pet_info(name='Боренька', animal_type='Кот', age=5):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_all_pets(auth_key, "my_pets")
+    pet_id = my_pets['pets'][4]['id']
+    status, result = pf.update_pet_info(auth_key, pet_id, name,animal_type,age)
+    if len(name and animal_type) > 0 and int(age):
+        assert result['animal_type'] == animal_type
+    else:
+        raise Exception('All fields must be completed')
